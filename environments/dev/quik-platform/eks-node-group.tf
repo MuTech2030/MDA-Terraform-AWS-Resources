a3c0f8ee2358cfdab085141bcb05e4e555aa7e70
Mustafa Sharif <mohamedmu2025@gmail.com>
2025-05-22 09:08:12 +0200

    eks node-group creation

--- environments/dev/quik-platform/eks-node-group.tf
+++ environments/dev/quik-platform/eks-node-group.tf
@@ -13,3 +13,16 @@ module "gpu_node_group" {
   policy_arns     = var.worker_policy_arns
   tags            = var.tags
 }
+module "apps_node_group" {
+  source          = "git@github.com:MuTech2030/MDA-Terraform-AWS-Modules.git//terraform-modules/09-eks-node-group"
+  cluster_name    = var.eks_control_plane_name
+  node_group_name = "apps-workers"
+  node_role_name  = "apps-node-role"
+  subnet_ids      = local.apps_node_group_subnet
+  desired_size    = 2
+  max_size        = 4
+  min_size        = 1
+  instance_types  = ["g4dn.xlarge"]
+  policy_arns     = var.worker_policy_arns
+  tags            = var.tags
+}
